beaac748dd628a233aae319b227d1093adadbcd0
Mohamed El-Sharqawy <dev.elbehery@gmail.com>
2024-05-25 06:07:42 +0300

    fix updating item bug

--- components/Resources/Resources.tsx
+++ components/Resources/Resources.tsx
@@ -72,7 +72,7 @@ export default function Resources() {
     const item = { cities: itemCities };
 
     const res = await fetch(
-      "api/update-item?categoryId=${updateCategoryID}&itemId=${mode}",
+      `api/update-item?categoryId=${updateCategoryID}&itemId=${mode}`,
       {
         method: "PATCH",
         body: JSON.stringify(item),
@@ -316,7 +316,7 @@ export default function Resources() {
                           </div>
                         </h4>
 
-                        {mode
+                        {mode && item._id.toString() === mode
                           ? updateCitiesData.map((city) => (
                               <div className="relative" key={city.name}>
                                 <Input
@@ -352,7 +352,7 @@ export default function Resources() {
                               </h4>
                             ))}
 
-                        {mode ? (
+                        {mode && item._id.toString() === mode ? (
                           <button
                             onClick={handleUpdate}
                             className="ml-2 text-white px-8 py-0.5 border-2 border-black dark:border-white uppercase bg-green-600 transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
